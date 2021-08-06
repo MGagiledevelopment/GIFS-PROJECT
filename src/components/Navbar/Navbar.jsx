189b4d3964c39faba6e2ec4d4b3f6e8c42cc72e9
MATIAS GONZALEZ <matias.goonzalez@gmail.com>
2021-08-06 16:38:32 -0300

    configuracion navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,10 +5,13 @@ import SearchButton from "./sub-components/search-button";
 export default function Navbar() {
   return (
     <div className={navbarStyles.navbar}>
+       <h1 className={navbarStyles.mainTitle}>¡Inpirate y busca los mejores gifts!</h1>
+        <img src="./images/navbar.svg"/>
       <div className={navbarStyles.sectionSearch}>
         <InputText />
         <SearchButton />
       </div>
+      <h2 className={navbarStyles.subTitle}>Realizá tu búsqueda</h2>
     </div>
   );
 }
