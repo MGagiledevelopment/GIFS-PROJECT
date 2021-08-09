f898c09c1575c233ff1e79aa68a4124d0df39b70
MATIAS GONZALEZ <matias.goonzalez@gmail.com>
2021-08-09 16:48:32 -0300

    arreglos css navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,18 +4,16 @@ import InputText from "./sub-components/input-text";
 import SearchButton from "./sub-components/search-button";
 
 export default function Navbar(props) {
-
-
   return (
     <div className={navbarStyles.navbar}>
-
-
-
-       <h1 className={`${props.button ? navbarStyles.dark : " " } ${navbarStyles.mainTitle} `}>¡Inpirate y busca los mejores gifts!</h1>
-
-
-
-        <img src="./images/navbar.svg"/>
+      <h1
+        className={`${props.button ? navbarStyles.dark : " "} ${
+          navbarStyles.mainTitle
+        } `}
+      >
+        ¡Inpirate y busca los mejores <span>GIFS</span>!
+      </h1>
+      <img src="./images/navbar.svg" className={navbarStyles.img}/>
       <div className={navbarStyles.sectionSearch}>
         <InputText />
         <SearchButton />
