c8a8fe6223622f89f2fcfa3d91e40f01e7404248
MATIAS GONZALEZ <matias.goonzalez@gmail.com>
2021-08-09 17:52:23 -0300

    arreglo size logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import Button from "../Header/sub-components/Button";
 export default function Header(props) {
   return (
     <header className={headerStyles.header}>
-      {(props.button) ? <img src="./images/logo-dark.svg" /> : <img src="./images/logo-light.svg" /> }
+      {(props.button) ? <img src="./images/logo-dark.svg" className={headerStyles.img}/> : <img src="./images/logo-light.svg" className={headerStyles.img}/> }
       <Button buttonHeader={props.button} setButtonHeader={props.setButton} />
     </header>
   );
