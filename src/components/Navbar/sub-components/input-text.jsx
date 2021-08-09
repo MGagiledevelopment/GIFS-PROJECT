66ba7194df96287c92daabd6edc63217d03d602d
MATIAS GONZALEZ <matias.goonzalez@gmail.com>
2021-08-09 17:36:33 -0300

    manejando estado input text

--- src/components/Navbar/sub-components/input-text.jsx
+++ src/components/Navbar/sub-components/input-text.jsx
@@ -1,6 +1,17 @@
-import React from "react"
+import React, {useState} from "react"
 import inputStyles from "../sub-components/inputs.module.css"
 
 export default function InputText (props){
-    return <input className={`${props.button ? inputStyles.dark : " "} ${inputStyles.inputtext} `}type="text" />
+   
+    const[text, setText] = useState("Buscar GIF")
+
+    const setterClick = ()=>{
+        setText(" ")
+    }
+
+    const setterText = (e)=>{
+        setText(e.target.value)
+    }
+
+    return <input value={text} onChange={setterText} onClick={setterClick} className={`${props.button ? inputStyles.dark : " "} ${inputStyles.inputtext} `}type="text" />
 }
