dfb23047c7cd6362b7d24eedc57bcad2242a28c2
MATIAS GONZALEZ <matias.goonzalez@gmail.com>
2021-08-09 12:11:45 -0300

    resolviendo modo dark

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import navbarStyles from "../Navbar/navbar.module.css";
 import InputText from "./sub-components/input-text";
 import SearchButton from "./sub-components/search-button";
+
+// import"../Navbar/dark.css"
 export default function Navbar(props) {
 
- 
+
   return (
     <div className={navbarStyles.navbar}>
 
 
 
-       <h1 className={navbarStyles.mainTitle}>¡Inpirate y busca los mejores gifts!</h1>
+       <h1 className={`${props.button ? navbarStyles.dark : " " } navbarStyles.mainTitle `}>¡Inpirate y busca los mejores gifts!</h1>
 
 
 
