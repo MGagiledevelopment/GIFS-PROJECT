c01e35a8d68daeb6903ef76e37e5a446fb5dd3e1
MATIAS GONZALEZ <matias.goonzalez@gmail.com>
2021-08-18 14:44:15 -0300

    logicas css

--- src/components/Gifts/Gifts.jsx
+++ src/components/Gifts/Gifts.jsx
@@ -4,32 +4,46 @@ import giftsStyles from "../Gifts/gifts.module.css";
 export default function Gifts(props) {
   return (
     <div className={giftsStyles.gifts}>
-      {props.array.length > 0
-        ? <p className={`${props.buttonHeader ? giftsStyles.dark : " "} ${giftsStyles.title2}`}>Resultado de tu búsqueda</p>
-        : <p className={`${props.buttonHeader ? giftsStyles.dark : " "} ${giftsStyles.title}`}>Realizá tu búsqueda</p>}
+      {props.array.length > 0 ? (
+        <p
+          className={`${props.buttonHeader ? giftsStyles.dark : " "} ${
+            giftsStyles.title2
+          }`}
+        >
+          Resultado de tu búsqueda
+        </p>
+      ) : (
+        <p
+          className={`${props.buttonHeader ? giftsStyles.dark : " "} ${
+            giftsStyles.title
+          }`}
+        >
+          Realizá tu búsqueda
+        </p>
+      )}
 
-
-
-    {props.array.length > 0 ? <div className={`${ props.buttonHeader ? giftsStyles.dark : " " } ${giftsStyles.boxGifts}`}>
-        {props.array.length > 0 ? (
-          props.array.map((gif) => {
-            return (
-              <img
-                src={gif.images.fixed_width.url}
-                className={giftsStyles.gif}
-              />
-            );
-          })
-        ) : (
-          <></>
-        )}
-      </div> : <></>}
-
-      
-
-
-
-
+      {props.array.length > 0 ? (
+        <div
+          className={`${props.buttonHeader ? giftsStyles.dark : " "} ${
+            giftsStyles.boxGifts
+          }`}
+        >
+          {props.array.length > 0 ? (
+            props.array.map((gif) => {
+              return (
+                <img
+                  src={gif.images.fixed_width.url}
+                  className={`${props.buttonHeader ? giftsStyles.dark : "" } ${giftsStyles.gif}`}
+                />
+              );
+            })
+          ) : (
+            <></>
+          )}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
