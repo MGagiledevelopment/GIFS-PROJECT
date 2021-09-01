da26c03cb98c8fc7a40736a5b8eb686829a39612
MATIAS GONZALEZ <matias.goonzalez@gmail.com>
2021-09-01 19:14:04 -0300

    trending styles

--- src/components/Gifts/Gifts.jsx
+++ src/components/Gifts/Gifts.jsx
@@ -15,30 +15,36 @@ export default function Gifts(props) {
         </p>
       ) : (
         <>
-        <p
-          className={`${props.buttonHeader ? giftsStyles.dark : " "} ${
-            giftsStyles.title
-          }`}
-        >
-          Realizá tu búsqueda
-        </p>
-        <p>TOP 15</p>
+          <p
+            className={`${props.buttonHeader ? giftsStyles.dark : " "} ${
+              giftsStyles.title
+            }`}
+          >
+            Realizá tu búsqueda
+          </p>
+          <p className={`${props.buttonHeader ? giftsStyles.dark : ""} ${giftsStyles.trendingtitle}`}> TOP TRENDING</p>
         </>
       )}
 
-      {props.array.length === 0 ? ( 
-        props.trending.map((gif) => {
-          return (
-            <a href="https://giphy.com">
-              <img
-                src={gif.images.fixed_width.url}
-                className={`${props.buttonHeader ? giftsStyles.dark : ""} ${
-                  giftsStyles.gif
-                }`}
-              />{" "}
-            </a>
-          );
-        })
+      {props.array.length === 0 ? (
+        <div
+          className={`${props.buttonHeader ? giftsStyles.dark : " "} ${
+            giftsStyles.boxGifts
+          }`}
+        >
+          {props.trending.map((gif) => {
+            return (
+              <a href="https://giphy.com">
+                <img
+                  src={gif.images.fixed_width.url}
+                  className={`${props.buttonHeader ? giftsStyles.dark : ""} ${
+                    giftsStyles.gif
+                  }`}
+                />{" "}
+              </a>
+            );
+          })}
+        </div>
       ) : (
         <></>
       )}
